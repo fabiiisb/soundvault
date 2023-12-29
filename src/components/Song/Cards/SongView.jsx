a4d320f94a8c24eafabfa0926b1abfe9cf71890d
Fabiiisb <fabiiisb@hotmail.com>
2023-12-29 16:57:47 -0300

    feat(songView): render condicional de etiquetas

--- src/components/Song/Cards/SongView.jsx
+++ src/components/Song/Cards/SongView.jsx
@@ -4,7 +4,7 @@ import { Image, Button, Tooltip } from '@nextui-org/react'
 import { ArchiveAdd, Timer1, Calendar, Play } from 'iconsax-react'
 import Link from 'next/link'
 
-export default function SongView ({ songName, user, src, duration, date, reproductions }) {
+export default function SongView ({ songName, user, src, duration, date, reproductions, songCount }) {
   return (
     <div className="border-none rounded-lg">
       <div className="grid grid-cols-6 minixl:grid-cols-12 gap-2 minixl:gap-6 items-center">
@@ -19,7 +19,8 @@ export default function SongView ({ songName, user, src, duration, date, reprodu
 
         <div className="flex flex-col col-span-6 sm:col-span-8 sm870:col-span-9 bg-content1/70 rounded-large shadow-medium p-2 px-3 h-full">
 
-          <h1 className="text-2xl minixl:text-3xl font-medium text-white truncate mb-1"
+          <h1
+            className="text-2xl minixl:text-3xl font-medium text-white truncate mb-1"
             title={songName}
           >
             {songName}
@@ -55,18 +56,37 @@ export default function SongView ({ songName, user, src, duration, date, reprodu
 
             </div>
             <div className='flex gap-2 flex-wrap text-white/70'>
-              <p className='flex items-center'>
-                <Timer1 className='h-4 text-white/90' />
-                {': ' + duration}
-              </p>
-              <p className='flex items-center'>
-                <Calendar className='h-4 text-white/90' />
-                {': ' + date}
-              </p>
-              <p className='flex items-center'>
-                <Play className='h-4 text-white/90' />
-                {': ' + reproductions}
-              </p>
+              {duration
+                ? <p className='flex items-center'>
+                  <Timer1 className='h-4 text-white/90' />
+                  {' ' + duration}
+                </p>
+                : ''
+              }
+
+              {date
+                ? <p className='flex items-center'>
+                  <Calendar className='h-4 text-white/90' />
+                  {' ' + date}
+                </p>
+                : ''
+              }
+
+              {reproductions
+                ? <p className='flex items-center'>
+                  <Play className='h-4 text-white/90' />
+                  {' ' + reproductions}
+                </p>
+                : ''
+              }
+
+              {songCount
+                ? <p className='flex items-center'>
+                  {songCount + ' songs'}
+                </p>
+                : ''
+              }
+
             </div>
           </div>
         </div>
