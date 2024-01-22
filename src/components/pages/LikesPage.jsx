cb9475bb25b3740cc49f57849c165943d10e8150
Fabiiisb <fabiiisb@hotmail.com>
2024-01-22 11:59:47 -0300

    feat(likes page): aviso en caso de estar vacio

--- src/components/pages/LikesPage.jsx
+++ src/components/pages/LikesPage.jsx
@@ -1,14 +1,15 @@
 'use client'
 import { SongUl } from '@/components/Song/SongList'
 import { useState, useEffect } from 'react'
-import { Skeleton } from '@nextui-org/react'
+import { Skeleton, Button } from '@nextui-org/react'
+import Link from 'next/link'
 
 const LikesPage = () => {
   const [songData, setSongData] = useState()
 
   useEffect(() => {
     fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/likes`,
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/private/likes`,
       {
         method: 'GET',
         headers: {
@@ -34,8 +35,31 @@ const LikesPage = () => {
     )
   }
 
+  if (songData.length === 0) {
+    return (
+      <div className='flex flex-col mt-5 font-semibold text-xl gap-5'>
+        <div className='flex flex-col'>
+          <span className='mx-auto'>
+            You don&apos;t have any songs added to your likes yet.
+          </span>
+          <span className='mx-auto text-niceOrange-400'>
+            Go explore and discover new songs!
+          </span>
+        </div>
+        <Button
+          as={Link}
+          href={'/'}
+          className='bg-content2 hover:text-niceOrange-400 mx-auto'
+        >
+          Go home
+        </Button>
+      </div>
+    )
+  }
+
   return (
     <div>
+      {console.log(songData)}
       <SongUl
         songList={songData}
         gradientColor={'from-bgBlur-700'}
