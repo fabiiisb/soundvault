d084ceb1c9ad3fab3fcf0fcc5a63761eacf240db
Fabiiisb <fabiiisb@hotmail.com>
2024-01-26 14:31:14 -0300

    feat(middleware)!: cambios menores middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,24 @@
-export { default } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+
+import { withAuth } from 'next-auth/middleware'
+
+export default withAuth(
+  function middleware (Request) {
+    const regex = /\/api\/*/
+
+    if (regex.test(Request.url)) {
+      console.log('Este mensaje deberia aparecer en cada ruta /api/*')
+    }
+
+    return NextResponse.next()
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token !== null
+    }
+  }
+)
 
 export const config = {
-  matcher: ['/test/:path*', '/private/:path*']
+  matcher: ['/test/:path*', '/private/:path*', '/api/private/:path*']
 }
