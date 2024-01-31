e74b6c5b988d58e454c3a989695a41c405831a4f
Fabiiisb <fabiiisb@hotmail.com>
2024-01-31 15:56:49 -0300

    feat(playerNav)!: seguimiento de cancion (btn)

--- src/components/Player/PlayerNav.jsx
+++ src/components/Player/PlayerNav.jsx
@@ -1,14 +1,16 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { BtnPrevSong, BtnNextSong, BtnPlaySong, BtnReplaySong, BtnRandomSong, BtnLikeSong, BtnVolume } from '@/components/Player/miniComp/PlayerBtns'
 import ProgressBar from './miniComp/ProgressBar'
+import playerContext from '@/context/MusicPlayer/playerContext'
 
 const MusicNav = () => {
-  const [play, setPlay] = useState(false)
   const [replay, setReplay] = useState(false)
   const [random, setRandom] = useState(false)
   const [liked, setLiked] = useState(false)
   const [isMute, setMute] = useState(false)
+
+  const { urlSong, activeSong } = useContext(playerContext)
 
   return (
     <nav className='bg-content1 text-white w-full fixed bottom-0 left-0 z-10 shadow-medium'>
@@ -22,7 +24,10 @@ const MusicNav = () => {
             setReplay={setReplay}
           />
           <BtnPrevSong />
-          <BtnPlaySong play={play} setPlay={setPlay} />
+          <BtnPlaySong
+            songId={activeSong}
+            songUrl={urlSong}
+          />
           <BtnNextSong />
           <BtnReplaySong
             replay={replay}
