4c849ac5073a537f4fafc618e14e46035cbbae19
Fabiiisb <fabiiisb@hotmail.com>
2024-03-20 15:41:49 -0300

    feat(get albums with songs): ahora los albumes ocultos son visibles para los dueños de el album

--- src/app/api/album/[albumId]/route.js
+++ src/app/api/album/[albumId]/route.js
@@ -1,15 +1,19 @@
 import { respMsgWithData } from '@/utils/respMsg'
 import { getConn } from '@/utils/db/dbConn'
 import { dbError } from '@/utils/db/dbErrors'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 import sql from 'mssql'
 
 export async function GET (request, { params }) {
   let pool
+  const session = await getServerSession(authOptions)
 
   try {
     pool = await getConn()
     const result = await pool.request()
       .input('ALBUM_ID', sql.Int, params.albumId)
+      .input('USER_ID', sql.VarChar(20), session?.user?.id)
       .execute('GetAlbumWithSongs')
 
     if (result.returnValue === 0) {
